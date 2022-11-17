ecf942855c574d1945f4ac0c11929f84eba937dd
Banuka Dananjaya <61711700+bannukadananjaya@users.noreply.github.com>
2022-11-17 18:10:20 +0530

    Unnecessary directory

--- New folder/admin/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-include('../config/constants.php'); 
-//1.Destroy the session
-session_destroy();//unset $_SESSION['user]
-
-//2.Redirect to login page
-header('location:'.SITEURL.'Resources/admin/login.php');
-?>
